13448554dfd04861c1ca14a28d8be79c064de09d
Andrii Savchenko <andrsvchnko@gmail.com>
2020-12-02 06:32:00 +0200

    Persist authentication

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import authReducer from "../features/auth/authSlice";
 
-export default configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+const rootReducer = combineReducers({
+  auth: authReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+
+  // https://github.com/rt2zz/redux-persist/issues/988
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ["persist/PERSIST"],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
