896420e79a0e9f7c9ac4a091b77725acd36dfbae
Andrii Savchenko <andrsvchnko@gmail.com>
2020-12-03 05:27:38 +0200

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 //import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-      </header>
-    </div>
+    <Router>
+      {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>*/}
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <React.Fragment></React.Fragment>}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
